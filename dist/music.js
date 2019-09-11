89dbdfc2a468a344cda1dd41e067879dfefebced
pasxsenger <478750937@qq.com>
2019-09-12 00:15:21 +0800

    Site updated: 2019-09-12 00:15:19

--- dist/music.js
+++ dist/music.js
@@ -14,6 +14,12 @@ const ap = new APlayer({
         artist: 'Ed Sheeran',
         url: 'http://m10.music.126.net/20190912002527/41d7115ea16bf3ed254f57303c4fc176/ymusic/c789/fdef/0831/8984d5b49bd9087185f8d341cc13ad55.mp3',
         cover: 'http://p1.music.126.net/coUnPtrHg0zUozRCwYOxog==/6051711999665662.jpg?param=130y130',
+      },
+      {
+        name: "어른 (大人)",
+        artist: '손디아 (Sondia)',
+        url: 'http://ws.stream.qqmusic.qq.com/C4001004TdwL4F73f6.m4a?guid=933646329&vkey=6A0E1F2ECBBF02D95B40F3282293E7F3671E00928B16C28417A62AA91A49032BEC293D26EE467C68752ABF31B12DF2794001A55046E36777&uin=0&fromtag=66',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000Mix2w1utUOr.jpg?max_age=2592000',
       }
     ]
 });
