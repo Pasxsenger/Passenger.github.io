14e40aee7c9484335e89e2b8a5d8a74d8467c381
pasxsenger <478750937@qq.com>
2019-09-12 23:03:16 +0800

    Site updated: 2019-09-12 23:03:14

--- dist/music.js
+++ dist/music.js
@@ -4,16 +4,10 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "Senorita (Acoustic)",
-        artist: 'Adam Christopher,Kimmy Nearon',
-        url: 'http://m10.music.126.net/20190912000829/863a53a7fa9b1820fac67f7f274c34c0/ymusic/515d/015b/555b/f7616fb116614a5cd8cfb19b63088d57.mp3',
+        name: "Senorita",
+        artist: 'Shawn Mendes,Camila Cabello',
+        url: 'http://ws.stream.qqmusic.qq.com/C400002n4xK13AJJvQ.m4a?guid=619010235&vkey=2D61CE8013A6217221FEC71855FDDF76C4981802F1DE1EF4CF3E497DE2E03FF30195E99F2A388A6A3CD92208844371EC9926684BF17D4A0D&uin=0&fromtag=66',
         cover: 'http://p1.music.126.net/tywnf5s0LaPMM640DKQTkQ==/109951164202370414.jpg?param=130y130',
-      },
-      {
-        name: "Thinking Out Loud",
-        artist: 'Ed Sheeran',
-        url: 'http://m10.music.126.net/20190912002527/41d7115ea16bf3ed254f57303c4fc176/ymusic/c789/fdef/0831/8984d5b49bd9087185f8d341cc13ad55.mp3',
-        cover: 'http://p1.music.126.net/coUnPtrHg0zUozRCwYOxog==/6051711999665662.jpg?param=130y130',
       },
       {
         name: "어른 (大人)",
